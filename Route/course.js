3b9b745767939d76311d9d491bffc3ebfe6d471f
Mrityunjay vyas <58501800+Mrityunjay383@users.noreply.github.com>
2022-04-10 21:33:30 +0530

    Update course.js

--- Route/course.js
+++ Route/course.js
@@ -10,11 +10,13 @@ router.get("/", isLogedIn, controller.allCourse);
 
 router.get("/addNewCourse", auth, isLogedIn, controller.addNewPage);
 
-router.post("/addNewCourse", auth,  controller.addNew);
+router.post("/addNewCourse", auth, controller.addNew);
 
 router.post("/join", auth, controller.join);
 
-router.get("/:courseID", auth, isLogedIn, controller.indiCourse);
+router.get("/myCourses", auth, isLogedIn, controller.myCourses);
+
+router.get("/:courseID", isLogedIn, auth, controller.indiCourse);
 
 
 module.exports = router;

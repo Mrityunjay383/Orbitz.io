26b4333b40c49e9a056f5b7c7d2e58c73cf21d13
Mrityunjay vyas <58501800+Mrityunjay383@users.noreply.github.com>
2022-04-10 21:38:32 +0530

    Update auth.js

--- Route/auth.js
+++ Route/auth.js
@@ -3,13 +3,17 @@ const router = express.Router();
 
 const controller = require('../Controller/auth');//Requring Controllers
 
-//Rendring login@Register Page
-router.get("/", controller.auth);
+const { isLogedIn, auth } = require('../Middleware/auth.js');//Requring Middleware isLogedIn
+
+//Rendring login&Register Page
+router.get("/", isLogedIn, controller.auth);
 
 //Login Route in /auth
 router.post("/login", controller.login);
 
 //Register Route in /auth
 router.post("/register", controller.register);
+
+router.get("/logout", controller.logout);
 
 module.exports = router;
